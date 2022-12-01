e5c00d0f2c1f9068076773b070a97ff2a8e5caf8
bu2terFLY <119470972+bu2terFLY@users.noreply.github.com>
2022-12-01 12:50:33 +0530

    Update inline.py

--- plugins/inline.py
+++ plugins/inline.py
@@ -103,7 +103,7 @@ async def answer(bot, query):
 def get_reply_markup(query):
     buttons = [
         [
-            InlineKeyboardButton('Search again', switch_inline_query_current_chat=query)
+            InlineKeyboardButton('ʙᴏᴛ ᴄʀᴇᴀᴛᴇᴅ ʙʏ', url='https://t.me/rb1bots')
         ]
         ]
     return InlineKeyboardMarkup(buttons)
